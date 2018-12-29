e10f2aa7b8da021ae8a285160f64695ad5bc7a72
itsvrushabh <itsvrushabh@gmail.com>
2018-12-29 19:53:41 +0530

    first commit

---
+++ pro1/blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+
+
+@login_required(redirect_field_name='')
+def home(request):
+	return render(request,'blog/home.html')
+
+def contact(request):
+	return render(request,'blog/contact.html')
+
+def about(request):
+	return render(request,'blog/about.html')
+
+def contributor(request):
+	return render(request, 'blog/contributor.html')
+
+def medium(request):
+	return render(request, 'blog/gitCard/medium.html')
